4ef3e8899766b196085452debffd0675681d534d
Farheen Malik <farheen.m.malik@gmail.com>
2014-10-29 10:23:30 -0400

    Add if-else method fromString back

--- src/Direction.java
+++ src/Direction.java
@@ -1,5 +1,29 @@
 
 abstract class Direction {	
+	
+	static Direction direction;
+	
+	static Direction fromString(String directionStr){
+		if (directionStr.equals("N")) {
+			direction = new North();
+		} else if (directionStr.equals("S")) {
+			direction = new South();
+		} else if (directionStr.equals("E")) {
+			direction = new East();
+		} else if (directionStr.equals("W")) {
+			direction = new West();
+		}
+		return direction;
+//		switch (direction) {
+//			case "N": return new North();
+//			case "S": return new South();
+//			case "E": return new East();
+//			case "W": return new West();
+////			default : return new North(); // needs to be redone
+//		}
+		//return new North(); // needs to be redone
+	}
+	
 	abstract int[] move(int[] currentPosition);
 	
 	abstract Direction turnLeft();
@@ -57,11 +81,11 @@ class East extends Direction {
 
 class West extends Direction {
 	Direction turnRight(){
-		return North();
+		return new North();
 	}
 	
 	Direction turnLeft(){
-		return South();
+		return new South();
 	}
 	
 	int[] move(int[] currentPosition){
