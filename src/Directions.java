16264e523685fe0c48decc6e4ae2ad2b1d6d0c97
Farheen Malik <farheen.m.malik@gmail.com>
2014-10-28 22:16:41 -0400

    Add abstract class for directions and then subclasses for directions

---
+++ src/Directions.java
@@ -0,0 +1,20 @@
+
+abstract class Directions {
+
+}
+
+class North extends Directions {
+	
+}
+
+class South extends Directions {
+	
+}
+
+class East extends Directions {
+	
+}
+
+class West extends Directions {
+	
+}
