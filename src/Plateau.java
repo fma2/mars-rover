8206ccd5b68e020c273ed2581504155236ea7cd2
Farheen Malik <farheen.m.malik@gmail.com>
2014-10-29 19:34:50 -0400

    Add convert string method to class, modify method so it actually turns the string into an array with the two coords

--- src/Plateau.java
+++ src/Plateau.java
@@ -6,5 +6,18 @@ public class Plateau {
 	public Plateau(int[] coordinates) {
 		plateauCoordinates = coordinates;
 	}
+	
+	static int[] convertString(String str) {
+		String[] items = str.split(" ");
+
+		int[] results = new int[items.length];
+	
+		for (int i = 0; i < items.length; i++) {
+		    try {
+		        results[i] = Integer.parseInt(items[i]);
+		    } catch (NumberFormatException nfe) {};
+		}
+		return results;
+	}
 
 }
