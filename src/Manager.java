4572e54b2fa2f3934021ab452225a63702c1781d
Farheen Malik <farheen.m.malik@gmail.com>
2014-10-28 22:18:57 -0400

    Remove main method from manager class

--- src/Manager.java
+++ src/Manager.java
@@ -8,6 +8,7 @@ public class Manager {
 	}
 	
 	public void direct(String instructions){
+		
 		//iterate over each character in instructions to see if L, R, or M
 		// not sure if need something that comes out of this...
 	}
