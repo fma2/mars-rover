0d5ef627e162cc6c0290663b893240a107f73530
Farheen Malik <farheen.m.malik@gmail.com>
2014-10-28 23:35:15 -0400

    Add switch statement for instructions stirng

--- src/Manager.java
+++ src/Manager.java
@@ -2,13 +2,19 @@
 public class Manager {
 	
 	Rover rover;
-	
+
 	public Manager(Rover newRover) {
 		rover = newRover;
 	}
-	
+
 	public void direct(String instructions){
-		
+		for (char ch: instructions.toCharArray()) {
+			switch (Character.toString(ch)) {
+				case "L": rover.turnLeft();
+				case "R": rover.turnRight();
+				case "M": rover.move();
+			}
+		}
 		//iterate over each character in instructions to see if L, R, or M
 		// not sure if need something that comes out of this...
 	}
