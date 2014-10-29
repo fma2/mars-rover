c9711258ad0e83a730ddab72ddf3abdf2c7adfb9
Farheen Malik <farheen.m.malik@gmail.com>
2014-10-28 22:16:19 -0400

    Add member variables and methods for turns and movement

--- src/rover.java
+++ src/rover.java
@@ -1,4 +1,22 @@
 
-public class rover {
-
+public class Rover {
+	public int[] position;
+	public String direction;
+	
+	public Rover(int[] currentPosition, String initialDirection) {
+		position = currentPosition;
+		direction = initialDirection;
+	}
+	
+//	public int[] move() {
+//	
+//	}
+//	
+//	public String turnLeft() {
+//		
+//	}
+//	
+//	public String turnRight() {
+//		
+//	}
 }
