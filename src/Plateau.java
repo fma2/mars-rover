7f4405ff1bf52b6286cca09c9dd7fea32f4d674d
Farheen Malik <farheen.m.malik@gmail.com>
2014-10-28 20:20:16 -0400

    Add Plateau class with constructor coordinates

---
+++ src/Plateau.java
@@ -0,0 +1,14 @@
+
+public class Plateau {
+	
+	public int[] plateauCoordinates;
+	
+	public Plateau(int[] coordinates) {
+		plateauCoordinates = coordinates;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
