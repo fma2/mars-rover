8c6a611fffed10b1ebcf1d775146fd5cdbe194e9
Farheen Malik <farheen.m.malik@gmail.com>
2014-10-28 22:18:15 -0400

    Add Manager class with member variable and initializer and direct method

---
+++ src/Manager.java
@@ -0,0 +1,15 @@
+
+public class Manager {
+	
+	Rover rover;
+	
+	public Manager(Rover newRover) {
+		rover = newRover;
+	}
+	
+	public void direct(String instructions){
+		//iterate over each character in instructions to see if L, R, or M
+		// not sure if need something that comes out of this...
+	}
+
+}
