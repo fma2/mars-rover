234710d3c78219b8af8429b037e084c388c78956
Farheen Malik <farheen.m.malik@gmail.com>
2014-10-28 23:55:08 -0400

    Add turn and move methods to south, east, west, classes

--- src/Direction.java
+++ src/Direction.java
@@ -25,12 +25,47 @@ class North extends Direction {
 
 class South extends Direction {
 	
+	Direction turnRight(){
+		return new West();
+	}
+	
+	Direction turnLeft(){
+		return new East();
+	}
+	
+	int[] move(int[] currentPosition){
+		currentPosition[1]--;
+		return currentPosition;
+	}
+	
 }
 
 class East extends Direction {
+	Direction turnRight(){
+		return new South();
+	}
 	
+	Direction turnLeft(){
+		return new North();
+	}
+	
+	int[] move(int[] currentPosition){
+		currentPosition[0]++;
+		return currentPosition;
+	}
 }
 
 class West extends Direction {
+	Direction turnRight(){
+		return North();
+	}
 	
+	Direction turnLeft(){
+		return South();
+	}
+	
+	int[] move(int[] currentPosition){
+		currentPosition[0]--;
+		return currentPosition;
+	}
 }
