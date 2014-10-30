7d4dc7640a66303ce77241b72196e0ac15623d4b
Farheen Malik <farheen.m.malik@gmail.com>
2014-10-29 21:27:01 -0400

    Add method to check if rover is out of bounds

--- src/Plateau.java
+++ src/Plateau.java
@@ -19,5 +19,24 @@ public class Plateau {
 		}
 		return results;
 	}
+	
+	boolean isRoverOutOfBounds(int[] roverPosition){
+		if (roverPosition[0] > plateauCoordinates[0]) {
+			return true;
+		}
+		
+		if (roverPosition[1] > plateauCoordinates[1]) {
+			return true;
+		}
+		
+		if (roverPosition[0] < 0){
+			return true;
+		}
+		
+		if (roverPosition[1] < 0) {
+			return true;
+		}
+		return false;
+	}
 
 }
