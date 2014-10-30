9f11e9b3e6e76294a1a284b8c2a1fe147c741bc9
Farheen Malik <farheen.m.malik@gmail.com>
2014-10-29 21:25:52 -0400

    Change exception type, remove commented out debugging code

--- src/RoverMain.java
+++ src/RoverMain.java
@@ -12,16 +12,11 @@ public class RoverMain {
 		int[] plateauCoordinates;
 		Plateau plateau;
 		
-		String roverPosition1;
-		int[] position1;
-		Direction direction1;
-		Rover rover1;
-		
-		String roverPosition2;
-		int[] position2;
-		Direction direction2;
-		Rover rover2;
-//		Manager manager;
+		String roverPosition;
+		int[] position;
+		Direction direction;
+		Rover rover;
+	
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(inputFileName)))
 		{
@@ -31,33 +26,15 @@ public class RoverMain {
 			plateauCoordinates = Plateau.convertString(br.readLine());
 			plateau = new Plateau(plateauCoordinates);
 			
-//			roverPosition1 = br.readLine();
-//			String[] parts1 = roverPosition1.split(" ");
-//			position1 = new int[] {Integer.valueOf(parts1[0]), Integer.valueOf(parts1[1])};
-//			direction1 = Direction.fromString(parts1[2]);
-//			rover1 = new Rover(position1, direction1);
-//			new Manager(rover1, plateau).direct(br.readLine());
-//			rover1.printStatus();
-//			
-//			roverPosition2 = br.readLine();
-//			String[] parts2 = roverPosition2.split(" ");
-//			position2 = new int[] {Integer.valueOf(parts2[0]), Integer.valueOf(parts2[1])};
-//			direction2 = Direction.fromString(parts2[2]);
-//			rover2 = new Rover(position2, direction2);
-//			new Manager(rover2, plateau).direct(br.readLine());
-//			rover2.printStatus();
-		
-			
 			while ((sCurrentLine = br.readLine()) != null) {						
-				roverPosition1 = sCurrentLine;
-				String[] parts1 = roverPosition1.split(" ");
-				position1 = new int[] {Integer.valueOf(parts1[0]), Integer.valueOf(parts1[1])};
-				direction1 = Direction.fromString(parts1[2]);
-				rover1 = new Rover(position1, direction1);
+				roverPosition = sCurrentLine;
+				String[] parts = roverPosition.split(" ");
+				position = new int[] {Integer.valueOf(parts[0]), Integer.valueOf(parts[1])};
+				direction = Direction.fromString(parts[2]);
+				rover = new Rover(position, direction);
 				
-				new Manager(rover1, plateau).direct(br.readLine());
-				rover1.printStatus();
-				//maybe the information above should go here?
+				new Manager(rover, plateau).direct(br.readLine());
+				rover.printStatus();
 			}
  
 		} catch (Exception e) {
