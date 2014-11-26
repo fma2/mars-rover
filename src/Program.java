3961c7e4f395a3da35f4f58ab95fd47f27515009
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-25 20:26:10 -0500

    Add code to convert first line of input into plateau coordinates

--- src/Program.java
+++ src/Program.java
@@ -12,6 +12,46 @@ public class Program {
 		int[] position;
 		Direction direction;
 		Rover rover;
+		
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(inputFileName)))
+		{
+ 
+			String sCurrentLine;
+			
+			plateauCoordinates = convertString(br.readLine());
+//			plateau = new Plateau(plateauCoordinates);
+			
+			while ((sCurrentLine = br.readLine()) != null) {						
+				roverPosition = sCurrentLine;
+				String[] parts = roverPosition.split(" ");
+//				if (parts.length != 3){
+//					throw new UnsupportedOperationException("Invalid input!");
+//				}
+//				position = new int[] {Integer.valueOf(parts[0]), Integer.valueOf(parts[1])};
+//				direction = Direction.fromString(parts[2]);
+//				rover = new Rover(position, direction);
+//				
+//				new Manager(rover, plateau).direct(br.readLine());
+//				rover.printStatus();
+			}
+ 
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	static int[] convertString(String str) {
+		String[] items = str.split(" ");
+
+		int[] results = new int[items.length];
+	
+		for (int i = 0; i < items.length; i++) {
+		    try {
+		        results[i] = Integer.parseInt(items[i]);
+		    } catch (NumberFormatException nfe) {};
+		}
+		return results;
 	}
 
 }
