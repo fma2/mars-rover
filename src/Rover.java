f38e5e6639dc8ad20a630d625b7662292142280d
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-28 14:40:09 -0500

    Change methods to call instance of position in direct method, comment out getters and setters for x and y, rover constructor create instance of position

--- src/Rover.java
+++ src/Rover.java
@@ -1,42 +1,41 @@
 
 public class Rover {
 
-	private int x;
-	private int y;	
-	private Direction direction;
+//	private int x;
+//	private int y;	
+//	private Direction direction;
 	private Position position;
 	
 	public Rover(int xCoordinate, int yCoordinate, Direction initialDirection) {
-//		this.position = new Position(xCoordinate, yCoordinate, initialDirection); 
-		x = xCoordinate;
-		y = yCoordinate;
-		direction = initialDirection;
+		this.position = new Position(xCoordinate, yCoordinate, initialDirection); 
+//		x = xCoordinate;
+//		y = yCoordinate;
+//		direction = initialDirection;
 	}
-	
-	void setDirection(Direction currentDirection) {
-		direction = currentDirection;
-	}
-	
-	Direction getDirection(){
-		return direction;
-	}
-	
-	
-	void setX(int xCoordinate) {
-		x = xCoordinate;
-	}
-	
-	int getX() {
-		return x;
-	}
-	
-	void setY(int yCoordinate) {
-		y = yCoordinate;
-	}
-	
-	int getY() {
-		return y;
-	}
+//	void setDirection(Direction currentDirection) {
+//		direction = currentDirection;
+//	}
+//	
+//	Direction getDirection(){
+//		return direction;
+//	}
+//	
+//	
+//	void setX(int xCoordinate) {
+//		x = xCoordinate;
+//	}
+//	
+//	int getX() {
+//		return x;
+//	}
+//	
+//	void setY(int yCoordinate) {
+//		y = yCoordinate;
+//	}
+//	
+//	int getY() {
+//		return y;
+//	}
 
 	void direct(Rover rover, String[] instructions){
 //		if (plateau.isRoverOutOfBounds(rover.position)){
@@ -44,39 +43,19 @@ public class Rover {
 //		}
 		for (int i=0; i < instructions.length; i++) {	
 			if(instructions[i].equals("L")){
-				Position.turnLeft(rover);
+				position.setDirectionLeft();
 			} else if(instructions[i].equals("R")){
-				Position.turnRight(rover);
-			} else if(instructions[i].equals("M")) {//&& getDirection().equals(Direction.NORTH) || getDirection().equals(Direction.SOUTH)){
-				Position.move(rover);
-//				
-//				switch (getDirection()) {
-//				case SOUTH:
-//					setY(y-1);
-//					break;
-//				case NORTH:
-//					setY(y+1);
-//					break;
-//				default:
-//					break;
-//				}
-//			} else if (instructions[i].equals("M") && getDirection().equals(Direction.EAST) || getDirection().equals(Direction.WEST)) {
-//				switch (getDirection()) {
-//				case WEST:
-//					setX(x-1);
-//					break;
-//				case EAST:
-//					setX(x+1);
-//					break;
-//				default:
-//					break;
-//				}
-////				rover.move();
-////				if (plateau.isRoverOutOfBounds(rover.position)){
-////					throw new UnsupportedOperationException("Rover out of bounds!!!");
-////				}
+				position.setDirectionRight();
+			} else if(instructions[i].equals("M")) {
+				position.move();
 			}
 		}
+	}
+	
+	public void getPosition(){
+		System.out.println(position.getX());
+		System.out.println(position.getY());
+		System.out.println(position.getDirection());
 	}
 
 }
