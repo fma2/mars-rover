35753564082c92d1903be8b991735d6c95d2df0e
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-28 14:44:19 -0500

    Refactoring of other files led to changes in Direction enum
    Add methods to each enum for left and right.  Add abstract methods left and right.

--- src/Direction.java
+++ src/Direction.java
@@ -1,18 +1,48 @@
 
 public enum Direction {
 	NORTH {
+		public Direction left() {
+			return WEST;
+		}
+		
+		public Direction right() {
+			return EAST;
+		}
 	},
 	
 	SOUTH {
+		public Direction left() {
+			return EAST;
+		}
+		
+		public Direction right() {
+			return WEST;
+		}
 		
 	},
 	
 	EAST {
+		public Direction left() {
+			return NORTH;
+		}
+		
+		public Direction right() {
+			return SOUTH;
+		}
 	},
 	
 	WEST {
+		public Direction left() {
+			return SOUTH;
+		}
 		
+		public Direction right() {
+			return NORTH;
+		}
 	},;
 	
+	public abstract Direction left();
+	public abstract Direction right();
+
 };
 
