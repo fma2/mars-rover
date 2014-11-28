618e57b1becb4223e893fcdb66db11bcb87311c3
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-28 13:33:43 -0500

    Add switch/case statement to method turnLeft - code from Rover class before

--- src/Position.java
+++ src/Position.java
@@ -1,10 +1,43 @@
 
-public interface Position {
-//	int[] move(int[] currentPosition);
-	
+//public interface Position {
+////	int[] move(int[] currentPosition);
+//	
 //	public abstract Direction turnLeft();
+//	
+////	public abstract Direction turnRight();
+//	
+//	
+//};
+
+public class Position {
+
+//	private int x;
+//	private int y;	
+//	private Direction direction;
 	
-//	public abstract Direction turnRight();
+	public Position (int xCoordinate, int yCoordinate, Direction direction) {
+//		this.x = xCoordinate;
+//		this.y = yCoordinate;
+//		this.direction = direction;
+	}
 	
+	public static void turnLeft(Rover rover) {
+		switch (rover.getDirection()) {
+		case EAST: 
+			rover.setDirection(Direction.NORTH);
+			break;
+		case NORTH:
+			rover.setDirection(Direction.WEST);
+			break;
+		case SOUTH:
+			rover.setDirection(Direction.EAST);
+			break;
+		case WEST:
+			rover.setDirection(Direction.SOUTH);
+			break;
+		default:
+			break;
+		}
+	}
 	
 }
