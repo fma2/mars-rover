9d63d458384720466858ef1eb0b1dfd5a120deaf
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-28 14:50:54 -0500

    Remove commented out methods for getters and setters, remove commented out constructor variables

--- src/Rover.java
+++ src/Rover.java
@@ -1,41 +1,11 @@
 
 public class Rover {
 
-//	private int x;
-//	private int y;	
-//	private Direction direction;
 	private Position position;
 	
 	public Rover(int xCoordinate, int yCoordinate, Direction initialDirection) {
 		this.position = new Position(xCoordinate, yCoordinate, initialDirection); 
-//		x = xCoordinate;
-//		y = yCoordinate;
-//		direction = initialDirection;
 	}
-//	void setDirection(Direction currentDirection) {
-//		direction = currentDirection;
-//	}
-//	
-//	Direction getDirection(){
-//		return direction;
-//	}
-//	
-//	
-//	void setX(int xCoordinate) {
-//		x = xCoordinate;
-//	}
-//	
-//	int getX() {
-//		return x;
-//	}
-//	
-//	void setY(int yCoordinate) {
-//		y = yCoordinate;
-//	}
-//	
-//	int getY() {
-//		return y;
-//	}
 
 	void direct(Rover rover, String[] instructions){
 //		if (plateau.isRoverOutOfBounds(rover.position)){
