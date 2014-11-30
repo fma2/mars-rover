27dac42c81d2bc6702da139e6ece926d14b90854
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-30 13:01:54 -0500

    Take away fixture again - it only makes one thing - so its not really saving that much memory

--- src/test/DirectionTest.java
+++ src/test/DirectionTest.java
@@ -2,43 +2,39 @@ package test;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 import main.Direction;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class DirectionTest {
 
-	Direction direction;
-	
-	@Before
-	public void setUp() throws Exception {
-		direction = Direction.NORTH;
-	}
-
 	@Test
 	public void testFromStringtoDirection() {
 		String north = "N";
-		Direction direction2 = Direction.fromStringToDirection(north);
-		assertThat(direction2, is(Direction.NORTH));
+		Direction direction = Direction.fromStringToDirection(north);
+		assertThat(direction, is(Direction.NORTH));
 	}
 	
 	@Test
 	public void testLeftChangesDirectionNorthToDirectionWest() {
+		Direction direction = Direction.NORTH; 
 		Direction changedDirection = direction.left();
 		assertThat(changedDirection, is(Direction.WEST));
 	}
 	
 	@Test
-	public void testRightChangesDirectionNorthToDirectionEast() {
+	public void testRightChangesDirectionEastToDirectionSouth() {
+		Direction direction = Direction.EAST; 
 		Direction changedDirection = direction.right();
-		assertThat(changedDirection, is(Direction.EAST));
+		assertThat(changedDirection, is(Direction.SOUTH));
 	}
 	
 	@Test
 	public void testToStringReturnsString() {
+		Direction direction = Direction.WEST;
 		String directionStr = direction.toString();
-		assertThat(directionStr, is("N"));
+		assertThat(directionStr, is("W"));
 	}
 
 }
