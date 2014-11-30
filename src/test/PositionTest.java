a16ae90b06cc5cba4e0d374f6623851e10361c0c
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-30 14:38:13 -0500

    Add tests to check if setDirection changes direction

--- src/test/PositionTest.java
+++ src/test/PositionTest.java
@@ -3,7 +3,6 @@ package test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import main.Direction;
-import main.Plateau;
 import main.Position;
 import main.Rover;
 
@@ -11,26 +10,26 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PositionTest {
-	
-	Rover rover;
-	Rover rover2;
-	Plateau plateau;
+
 	int x;
 	int y;
+	Rover rover;
+	Rover rover2;
+	Position position;
+	Position position2;
 
 	@Before
 	public void setUp() throws Exception {
 		x = 1;
 		y = 2;
 		rover = new Rover(x,y, Direction.EAST);
+		position = rover.getPosition();
 		rover2 = new Rover(x,y, Direction.NORTH);
-		int[] plateauCoordinates = {5,5};
-		plateau = new Plateau(plateauCoordinates);
+		position2 = rover2.getPosition();
 	}
 
 	@Test
 	public void testMoveDoesNotChangeDirection() {
-		Position position = rover.getPosition();
 		Direction direction = position.getDirection();
 		position.move();
 		Direction newDirection = position.getDirection();
@@ -39,7 +38,6 @@ public class PositionTest {
 
 	@Test
 	public void testMoveChangesXCoordinateByOneWhenDirectionIsEast(){
-		Position position = rover.getPosition();
 		int xCoordinate = position.getX();
 		position.move();
 		int newXCoordinate = position.getX();
@@ -48,11 +46,25 @@ public class PositionTest {
 	
 	@Test
 	public void testMoveChangesYCoordinateByOneWhenDirectionIsNorth(){
-		Position position = rover2.getPosition();
-		int yCoordinate = position.getY();
-		position.move();
-		int newYCoordinate = position.getY();
+		int yCoordinate = position2.getY();
+		position2.move();
+		int newYCoordinate = position2.getY();
 		assertThat(newYCoordinate - 1, is((yCoordinate)));
+	}
+	
+	@Test
+	public void testSetDirectionRightChangesDirection() {
+		Direction direction = position.getDirection();
+		position.setDirectionRight();
+		Direction newDirection = position.getDirection();
+		assertThat(newDirection, is(not(direction)));
+	}
+	
+	@Test 
+	public void testSetDirectionLeftChangedDirectionNorthtoWest(){
+		position2.setDirectionLeft();
+		Direction newDirection = position2.getDirection();
+		assertThat(newDirection, is(Direction.WEST));
 	}
 	
 }
