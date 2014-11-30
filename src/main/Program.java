124498f275593c760e84fff25baa6a0037909bec
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-30 12:48:10 -0500

    Move some calls to make variables from outside buffered reader to inside next to pointer for that variable

--- src/main/Program.java
+++ src/main/Program.java
@@ -5,47 +5,38 @@ public class Program {
 
 	public static void main(String[] args) {
 		String inputFileName = "src/input.txt";
-
-		int[] plateauCoordinates;
+		
+//		if (args.length == 1) {
+//			inputFileName = args[0];
+//		}
+		
 		Plateau plateau;
-		
-		String roverPosition;
-		int xCoordinate;
-		int yCoordinate;
-
-		Direction direction;
 		Rover rover;
-		
 		String[] instructions;
-		String[] parsedInstructions;
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(inputFileName)))
-		{
+		try (BufferedReader br = new BufferedReader(new FileReader(inputFileName))) {
  
 			String sCurrentLine;
 			
-			plateauCoordinates = Plateau.convertStringToIntArray(br.readLine());
+			int[] plateauCoordinates = Plateau.convertStringToIntArray(br.readLine());
 			plateau = new Plateau(plateauCoordinates);
 			
 			while ((sCurrentLine = br.readLine()) != null) {						
-				roverPosition = sCurrentLine;
+				String roverPosition = sCurrentLine;
 				String[] parts = roverPosition.split(" ");
 				if (parts.length != 3){
 					throw new UnsupportedOperationException("Invalid input!");
 				}
-				xCoordinate = Integer.valueOf(parts[0]);
-				yCoordinate = Integer.valueOf(parts[1]);
-				direction = Direction.fromStringToDirection(parts[2]);
 				
+				int xCoordinate = Integer.valueOf(parts[0]); int yCoordinate = Integer.valueOf(parts[1]);
+				Direction direction = Direction.fromStringToDirection(parts[2]);
 				rover = new Rover(xCoordinate, yCoordinate, direction);
 				
 				instructions = br.readLine().split("");
 				
 				rover.direct(instructions);
-				rover.printPosition();				
-				
+				rover.printPosition();					
 			}
- 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
