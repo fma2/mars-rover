ee5c85cb61bbd0d6be8a5d9ace7e8bcdb0d1fc22
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-29 22:11:28 -0500

    Move method convertStringtoIntArray from Program to Plateau

--- src/main/Plateau.java
+++ src/main/Plateau.java
@@ -8,7 +8,20 @@ public class Plateau {
 		plateauCoordinates = coordinates;
 	}
 	
-//	boolean isRoverOutOfBounds(int[] roverPosition){
+	static int[] convertStringToIntArray(String str) {
+		String[] items = str.split(" ");
+
+		int[] results = new int[items.length];
+	
+		for (int i = 0; i < items.length; i++) {
+		    try {
+		        results[i] = Integer.parseInt(items[i]);
+		    } catch (NumberFormatException nfe) {};
+		}
+		return results;
+	}
+	
+//	public boolean isRoverOutOfBounds(int[] roverPosition){
 //		if (roverPosition[0] > plateauCoordinates[0]) {
 //			return true;
 //		}
