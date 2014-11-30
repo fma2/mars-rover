e3a2cc307c819c92563265204c5d76cd0b704cb6
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-30 12:49:17 -0500

    Remove methods before and after and their imports because not using

--- src/test/DirectionTest.java
+++ src/test/DirectionTest.java
@@ -3,35 +3,11 @@ package test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
-import java.lang.reflect.Method;
-
 import main.Direction;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DirectionTest {
-
-	private static final String String = null;
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
 
 	@Test
 	public void testFromStringtoDirection() {
@@ -58,7 +34,7 @@ public class DirectionTest {
 	public void testToStringReturnsString() {
 		Direction direction = Direction.WEST;
 		String directionStr = direction.toString();
-		assertThat(directionStr, is("W"));
+		assertThat(directionStr, is(""));
 	}
 
 }
