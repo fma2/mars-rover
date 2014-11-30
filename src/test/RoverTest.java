2d415214e12c08a5fca54099a77bcb5fb3fe7b15
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-30 13:47:22 -0500

    Add tests for exceptions for when rover is out of bounds in direct method

--- src/test/RoverTest.java
+++ src/test/RoverTest.java
@@ -9,6 +9,7 @@ import java.io.PrintStream;
 import org.junit.Before;
 import org.junit.Test;
 
+import main.Plateau;
 import main.Position;
 import main.Rover;
 import main.Direction;
@@ -19,6 +20,7 @@ public class RoverTest {
 	Rover rover;
 	Rover rover2;
 	Position position;
+	Plateau plateau;
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
 	@Before
@@ -26,6 +28,8 @@ public class RoverTest {
 		rover = new Rover(1,2,Direction.NORTH);
 		rover2 = new Rover(3,3, Direction.NORTH);
 		position = rover.getPosition();
+		int[] plateauCoordinates = {5, 5};
+		plateau = new Plateau(plateauCoordinates);
 		System.setOut(new PrintStream(outContent));
 	}
 	
@@ -43,7 +47,8 @@ public class RoverTest {
 	@Test
 	public void testDirectMovesRover() {
 		int beforeDirectXCoordinate = rover.getPosition().getX();
-		rover.direct("LMLMLMMMM".split(""));
+		String[] instructions = "RM".split(""); 
+		rover.direct(instructions, plateau);
 		Position afterDirectPosition = rover.getPosition();
 		assertThat(beforeDirectXCoordinate, is(not(afterDirectPosition.getX())));
 	}
@@ -52,6 +57,17 @@ public class RoverTest {
 	public void testPrintPosition(){
 		rover2.printPosition();
 		assertThat("3 3 N", is(outContent.toString().trim()));
+	}
+	
+	@Test(expected=UnsupportedOperationException.class)
+	public void testExceptionRoverisOutofBoundsatBeginningofInstructions() {
+		Rover rover3 = new Rover(6, 6, Direction.NORTH);
+		rover3.direct("LMMMMM".split(""), plateau);
+	}
+	
+	@Test(expected=UnsupportedOperationException.class)
+	public void testExceptionRoverisOutofBoundsatEndofMove() {
+		rover2.direct("LMMMMM".split(""), plateau);		
 	}
 
 }
