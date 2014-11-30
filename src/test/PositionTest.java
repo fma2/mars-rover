eba6bf336ce77d75dc9e78c1a77622b42da496db
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-30 14:25:18 -0500

    Add tests for position class
    Tests for move not changing the direction.  Tests for move on x and y.  Remove after, afterclass, and beforeclass.

--- src/test/PositionTest.java
+++ src/test/PositionTest.java
@@ -1,34 +1,58 @@
 package test;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import main.Direction;
+import main.Plateau;
+import main.Position;
+import main.Rover;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PositionTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	
+	Rover rover;
+	Rover rover2;
+	Plateau plateau;
+	int x;
+	int y;
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		x = 1;
+		y = 2;
+		rover = new Rover(x,y, Direction.EAST);
+		rover2 = new Rover(x,y, Direction.NORTH);
+		int[] plateauCoordinates = {5,5};
+		plateau = new Plateau(plateauCoordinates);
 	}
 
 	@Test
-	public void test() {
-//		fail("Not yet implemented");
+	public void testMoveDoesNotChangeDirection() {
+		Position position = rover.getPosition();
+		Direction direction = position.getDirection();
+		position.move();
+		Direction newDirection = position.getDirection();
+		assertThat(newDirection, is(direction));	
 	}
 
+	@Test
+	public void testMoveChangesXCoordinateByOneWhenDirectionIsEast(){
+		Position position = rover.getPosition();
+		int xCoordinate = position.getX();
+		position.move();
+		int newXCoordinate = position.getX();
+		assertThat(newXCoordinate - 1, is((xCoordinate)));
+	}
+	
+	@Test
+	public void testMoveChangesYCoordinateByOneWhenDirectionIsNorth(){
+		Position position = rover2.getPosition();
+		int yCoordinate = position.getY();
+		position.move();
+		int newYCoordinate = position.getY();
+		assertThat(newYCoordinate - 1, is((yCoordinate)));
+	}
+	
 }
