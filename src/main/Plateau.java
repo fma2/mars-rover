0c5578e4db9aab2f6fe813b5f3bcba376febdc25
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-29 20:20:24 -0500

    Add plateau class

---
+++ src/main/Plateau.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Plateau {
+
+	private int[] plateauCoordinates;
+
+	public Plateau (int[] coordinates){
+		plateauCoordinates = coordinates;
+	}
+	
+//	boolean isRoverOutOfBounds(int[] roverPosition){
+//		if (roverPosition[0] > plateauCoordinates[0]) {
+//			return true;
+//		}
+//		
+//		if (roverPosition[1] > plateauCoordinates[1]) {
+//			return true;
+//		}
+//		
+//		if (roverPosition[0] < 0){
+//			return true;
+//		}
+//		
+//		if (roverPosition[1] < 0) {
+//			return true;
+//		}
+//		return false;
+//	}
+}
