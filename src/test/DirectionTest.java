b3d4d6bb138f295ce6059ebd58adb4f412428336
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-30 13:00:45 -0500

    Add fixture for direction, modify tests to use fixture instead of creating new direction

--- src/test/DirectionTest.java
+++ src/test/DirectionTest.java
@@ -2,39 +2,43 @@ package test;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
-
 import main.Direction;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class DirectionTest {
 
+	Direction direction;
+	
+	@Before
+	public void setUp() throws Exception {
+		direction = Direction.NORTH;
+	}
+
 	@Test
 	public void testFromStringtoDirection() {
 		String north = "N";
-		Direction direction = Direction.fromStringToDirection(north);
-		assertThat(direction, is(Direction.NORTH));
+		Direction direction2 = Direction.fromStringToDirection(north);
+		assertThat(direction2, is(Direction.NORTH));
 	}
 	
 	@Test
 	public void testLeftChangesDirectionNorthToDirectionWest() {
-		Direction direction = Direction.NORTH; 
 		Direction changedDirection = direction.left();
 		assertThat(changedDirection, is(Direction.WEST));
 	}
 	
 	@Test
-	public void testRightChangesDirectionEastToDirectionSouth() {
-		Direction direction = Direction.EAST; 
+	public void testRightChangesDirectionNorthToDirectionEast() {
 		Direction changedDirection = direction.right();
-		assertThat(changedDirection, is(Direction.SOUTH));
+		assertThat(changedDirection, is(Direction.EAST));
 	}
 	
 	@Test
 	public void testToStringReturnsString() {
-		Direction direction = Direction.WEST;
 		String directionStr = direction.toString();
-		assertThat(directionStr, is(""));
+		assertThat(directionStr, is("N"));
 	}
 
 }
