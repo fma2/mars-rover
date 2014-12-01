087868022b7872c743bc3964a6df2fb9b851e8e9
Farheen Malik <farheen.m.malik@gmail.com>
2014-11-30 22:31:37 -0500

    Add system out print information
    Add line showing number of tests that are run.  Add dividing lines between number of tests run, failed tests, and whether or not all tests were successful.

--- src/test/TestRunner.java
+++ src/test/TestRunner.java
@@ -5,11 +5,15 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class TestRunner {
-   public static void main(String[] args) {
+	public static void main(String[] args) {
       Result result = JUnitCore.runClasses(AllTests.class);
+      System.out.println("# of tests: "+result.getRunCount()+"");
+      System.out.println("=======================");
       for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
+         System.out.println("Failed test: "+failure.toString()+"");
       }
-      System.out.println(result.wasSuccessful());
+      System.out.println("=======================");
+      System.out.println("All tests were successful?: "+result.wasSuccessful()+"");
    }
+
 } 
