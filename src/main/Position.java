5a9a076bcbf51eefd71b3247401ae25b1c7f287b
Farheen Malik <farheen.m.malik@gmail.com>
2015-01-15 00:52:51 -0500

    Comment out  methods that refer to direction, now only using this class to store coordinates

--- src/main/Position.java
+++ src/main/Position.java
@@ -4,27 +4,25 @@ public class Position {
 
 	private int x;
 	private int y;	
-	private Direction direction;
 	
-	public Position (int xCoordinate, int yCoordinate, Direction direction) {
+	public Position (int xCoordinate, int yCoordinate) {
 		this.x = xCoordinate;
 		this.y = yCoordinate;
-		this.direction = direction;
 	}
 	
 	// Sets the direction of the rover to turn right from the current direction
-	public void setDirectionRight(){
-		direction = direction.right();
-	}
+//	public void setDirectionRight(){
+//		direction = direction.right();
+//	}
 	
 	// Sets the direction of the rover to turn left from the current direction
-	public void setDirectionLeft() {
-		direction = direction.left();
-	}
+//	public void setDirectionLeft() {
+//		direction = direction.left();
+//	}
 	
-	public Direction getDirection() {
-		return direction;
-	}
+//	public Direction getDirection() {
+//		return direction;
+//	}
 	
 	public void setX(int xCoordinate) {
 		x = xCoordinate;
@@ -43,22 +41,22 @@ public class Position {
 	}
 
 	// Moves the rover along x or y depending on its current direction
-	public void move() {
-		switch (getDirection()) {
-		case NORTH:
-			y++;
-			break;
-		case SOUTH:
-			y--;
-			break;
-		case EAST:
-			x++;
-			break;
-		case WEST:
-			x--;
-			break;
-		default:
-			break;
-		}
-	}
+//	public void move() {
+//		switch (getDirection()) {
+//		case NORTH:
+//			y++;
+//			break;
+//		case SOUTH:
+//			y--;
+//			break;
+//		case EAST:
+//			x++;
+//			break;
+//		case WEST:
+//			x--;
+//			break;
+//		default:
+//			break;
+//		}
+//	}
 }
