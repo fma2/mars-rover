34482016071149b60c2c9ac418d2d279bd6c7eb8
Farheen Malik <farheen.m.malik@gmail.com>
2015-01-15 00:54:19 -0500

    Add methods turnLeft, turnRight, move, change member variables in constructor to parse out direction and position/coords; still working on this

--- src/main/Rover.java
+++ src/main/Rover.java
@@ -1,40 +1,71 @@
 package main;
 
+import java.util.*;
+
+import commands.*;
+
 // The Rover object with a position field
 public class Rover {
 
-	private Position position;
+//	private Position position;
 	
-	public Rover(int xCoordinate, int yCoordinate, Direction initialDirection) {
-		this.position = new Position(xCoordinate, yCoordinate, initialDirection); 
+	private Position currentPosition;
+	private Direction currentDirection;
+	private Plateau plateau;
+	
+	public Rover(Plateau plateau, Position position, Direction direction) {
+		this.plateau = plateau;
+		this.currentPosition = position;
+		this.currentDirection = direction;
+//		this.position = new Position(xCoordinate, yCoordinate, initialDirection); 
 	}
 
 	// Takes string of instructions input and parses to move the the rover
-	public void direct(String[] instructions, Plateau plateau){
-		if (plateau.isRoverOutOfBounds(position)){
-			throw new UnsupportedOperationException("Rover out of bounds!!!");
-		}
-		for (int i=0; i < instructions.length; i++) {	
-			if(instructions[i].equals("L")){
-				position.setDirectionLeft();
-			} else if(instructions[i].equals("R")){
-				position.setDirectionRight();
-			} else if(instructions[i].equals("M")) {
-				position.move();
-				if (plateau.isRoverOutOfBounds(position)){
-					throw new UnsupportedOperationException("Rover out of bounds!!!");
-				}
-			}
+	public void direct(List<ICommand> instructions){
+//		if (plateau.isRoverOutOfBounds(position)){
+//			throw new UnsupportedOperationException("Rover out of bounds!!!");
+//		}
+		for (ICommand command : instructions) {	
+			command.execute(this);
 		}
 	}
 	
-	// Get information on the position of the rover
-	public Position getPosition(){
-		return position;
+	public void turnLeft() {
+		currentDirection = currentDirection.left();
 	}
+	public void turnRight() {
+		currentDirection = currentDirection.right();
+	}
+	
+	public void move() {
+		//below needs to exist in position class!
+		switch (this.currentDirection) {
+		case NORTH:
+			int y = currentPosition.getY();
+			currentPosition.setY(y+1);
+			break;
+//		case SOUTH:
+//			y--;
+//			break;
+//		case EAST:
+//			x++;
+//			break;
+//		case WEST:
+//			x--;
+//			break;
+		default:
+			break;
+		}
+	}
+	
+	
+	// Get information on the position of the rover
+//	public Position getPosition(){
+//		return position;
+//	}
 	
 	// Print the rover's position to the console
 	public void printPosition() {
-		System.out.println(""+position.getX()+" "+position.getY()+" " +position.getDirection()+"");
+//		System.out.println(""+position.getX()+" "+position.getY()+" " +position.getDirection()+"");
 	}
 }
