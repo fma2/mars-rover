a6886bce00bd2632c839daa75c937ececb765418
Farheen Malik <farheen.m.malik@gmail.com>
2015-01-15 22:00:39 -0500

    Change argument names for getters and setters and constructor to just x and y

--- src/main/Plateau.java
+++ src/main/Plateau.java
@@ -6,21 +6,21 @@ public class Plateau {
 	private int xCoordinate;
 	private int yCoordinate;
 
-	public Plateau (int xCoordinate, int yCoordinate){
-		this.xCoordinate = xCoordinate;
-		this.yCoordinate = yCoordinate;
+	public Plateau (int x, int y){
+		this.xCoordinate = x;
+		this.yCoordinate = y;
 	}
 	
-	public void setX(int xCoordinate) {
-		xCoordinate = xCoordinate;
+	public void setX(int x) {
+		xCoordinate = x;
 	}
 
 	public int getX() {
 		return xCoordinate;
 	}
 	
-	public void setY(int yCoordinate) {
-		yCoordinate = yCoordinate;
+	public void setY(int y) {
+		yCoordinate = y;
 	}
 
 	public int getY() {
