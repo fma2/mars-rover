8c8024e5a20a4f409bcd9b94497242d86acf81fc
Farheen Malik <farheen.m.malik@gmail.com>
2015-02-02 20:33:30 -0500

    Add methods for roverDeath, getIsAlive, and add alive status to print and add leaveBeacon in direct method

--- src/main/Rover.java
+++ src/main/Rover.java
@@ -4,30 +4,38 @@ package main;
 public class Rover {
 
 	private Position position;
+	private boolean isAlive;
 	
 	public Rover(int xCoordinate, int yCoordinate, Direction initialDirection) {
-		this.position = new Position(xCoordinate, yCoordinate, initialDirection); 
+		this.position = new Position(xCoordinate, yCoordinate, initialDirection);
+		this.isAlive = true;
 	}
 
 	// Takes string of instructions input and parses to move the the rover
 	public void direct(String[] instructions, Plateau plateau){
-		if (plateau.isRoverOutOfBounds(position)){
-			throw new UnsupportedOperationException("Rover out of bounds!!!");
-		}
 		for (int i=0; i < instructions.length; i++) {	
 			if(instructions[i].equals("L")){
 				position.setDirectionLeft();
 			} else if(instructions[i].equals("R")){
 				position.setDirectionRight();
 			} else if(instructions[i].equals("M")) {
+				
 				position.move();
 				if (plateau.isRoverOutOfBounds(position)){
-					throw new UnsupportedOperationException("Rover out of bounds!!!");
+					plateau.leaveBeacon(position);
+					roverDeath();
 				}
 			}
 		}
 	}
 	
+	public void roverDeath() {
+		isAlive = false;
+	}
+	
+	public boolean getIsAlive() {
+		return isAlive;
+	}
 	// Get information on the position of the rover
 	public Position getPosition(){
 		return position;
@@ -35,6 +43,7 @@ public class Rover {
 	
 	// Print the rover's position to the console
 	public void printPosition() {
-		System.out.println(""+position.getX()+" "+position.getY()+" " +position.getDirection()+"");
+		if (getIsAlive() == false) System.out.println(""+position.getX()+" "+position.getY()+" " +position.getDirection()+"" + " RIP");
+		else System.out.println(""+position.getX()+" "+position.getY()+" " +position.getDirection()+"");
 	}
 }
